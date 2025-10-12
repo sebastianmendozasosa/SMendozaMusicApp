d94cfc6092e168e4f0f5d6bffa6b870e35fa17a0
Sebastián Mendoza Sosa <sms79250@lasallebajio.edu.mx>
2025-10-12 00:47:06 -0600

    Creacion de CurrPlayCard.kt, esta se reflejara en homescreen

--- app/src/main/java/com/example/musicapp/components/CurrPlayCard.kt
+++ app/src/main/java/com/example/musicapp/components/CurrPlayCard.kt
@@ -1,12 +1,9 @@
 package com.example.musicapp.components
 
-import android.R.attr.tint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,10 +12,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,78 +25,80 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.example.musicapp.models.Album
 import com.example.musicapp.ui.theme.MusicAppTheme
+import com.example.musicapp.ui.theme.Purple80
+
 
 @Composable
-fun AlbumCard(
+fun CurrPlayCard(
     album: Album,
-    onClick : () -> Unit
 ){
-    Box(
+Card(
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp, vertical = 8.dp),
+    shape = RoundedCornerShape(24.dp),
+    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+    colors = CardDefaults.cardColors(containerColor = Color(0xFF2E004F).copy(alpha = 0.8f))
+    ) {
+    Row(
         modifier = Modifier
-            .width(200.dp)
-            .height(180.dp)
-            .clip(RoundedCornerShape(16.dp))
-            .clickable{
-                onClick()
-            },
+            .padding(12.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+
     ) {
         AsyncImage(
             model = album.image,
             contentDescription = album.title,
             modifier = Modifier
-                .fillMaxSize()
-                .clip(RoundedCornerShape(16.dp)),
+                .size(60.dp)
+                .clip(RoundedCornerShape(8.dp)),
             contentScale = ContentScale.Crop
         )
 
-        Row(
-            modifier = Modifier
-                .align (Alignment.BottomStart)
-                .fillMaxWidth()
-                .background(Color(0xFF2E004F).copy(alpha = 0.8f))
-                .clip(RoundedCornerShape(16.dp))
-                .padding(horizontal = 12.dp, vertical = 8.dp),
-            verticalAlignment = Alignment.CenterVertically,
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Column(
+            modifier = Modifier.weight(1f)
         ) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-            ) {
-                Text(
-                    text = album.title,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = Color.White,
-                    maxLines = 1
-                )
+            Text(
+                text = album.title,
+                style = MaterialTheme.typography.titleSmall,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Text(
                     text = album.artist,
                     style = MaterialTheme.typography.bodySmall,
-                    color = Color.White,
+                    color = Color.White
                 )
+                Spacer(modifier = Modifier.width(6.dp))
+
             }
-            Icon(
-                imageVector = Icons.Default.PlayArrow,
-                contentDescription = "Play",
-                tint = Color.Black,
-                modifier = Modifier
-                    .size(32.dp)
-                    .background(Color.White, shape = CircleShape)
-                    .padding(4.dp)
-            )
+        }
+        IconButton(
+            onClick = {},
+            modifier = Modifier
+                .background(Color.White, CircleShape)
+                .size(40.dp)
+        ) {
+            Icon(Icons.Default.PlayArrow, contentDescription = "Play", tint = Color(0xFF2E004F))
+        }
         }
     }
 }
 
+
 @Preview
 @Composable
-fun AlbumCardPreview(){
+fun CurrPlayCardPreview(){
     val sampleAlbum = Album(
         title = "Sample Album",
         artist = "Sample Artist",
@@ -106,11 +107,9 @@ fun AlbumCardPreview(){
         id = "qewfasedfcsdvsa"
     )
     MusicAppTheme {
-        AlbumCard(
+        CurrPlayCard(
             album = sampleAlbum,
-            onClick = { }
         )
     }
 
 }
-
